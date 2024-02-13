d19a55b4c39cb29bfbbca08091e2e55b3dd67e82
KolyaShabalinskiy <kolya.shabalinskiy@gmail.com>
2024-02-13 08:15:21 +0300

    create services

---
+++ TestTask/Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class OrderService : IOrderService
+    {
+        ApplicationDbContext dbContext;
+        public OrderService(ApplicationDbContext Context)
+        {
+            dbContext = Context;
+        }
+        public async Task<Order> GetOrder()
+        { 
+            //var res = dbContext.Orders.MaxBy(o => o.Price * o.Quantity);
+            //return res;
+
+            var resultOrder = from order in dbContext.Orders
+                              where order.Price * order.Quantity == (from or in dbContext.Orders
+                                                                     select or.Quantity*or.Price).Max()
+                              select order;
+            
+            
+            return await resultOrder.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            return await dbContext.Orders.Where(q => q.Quantity > 10).ToListAsync();
+        }
+    }
+}
---
+++ TestTask/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Enums;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services
+{
+    public class UserService : IUserService
+    {
+        private ApplicationDbContext dbContext;
+        public UserService(ApplicationDbContext applicationDb)
+        {
+            dbContext = applicationDb;
+        }
+        public async Task<User> GetUser()
+        {
+            //var result = dbContext.Users.MaxBy(x => x.Orders.Count()); // Здравствйте!
+            //return result;                                             // MaxBy() можно было использовать?         
+
+            var result = from user in dbContext.Users
+                         where user.Orders.Count == (from or in dbContext.Orders
+                                                     group or.UserId by or.UserId into g
+                                                     select new { Name = g.Key, Count = g.Count() })
+                                                     .Max( u => u.Count)
+                         select user;           
+            
+            return await result.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            return await dbContext.Users.Where(s => s.Status == UserStatus.Inactive).ToListAsync();
+        }
+    }
+}
